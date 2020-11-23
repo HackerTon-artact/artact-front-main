1d7b842d99e2129c770226cf317b6ab789311451
최종근 <zofqofhtltm2@gmail.com>
2020-11-23 23:36:56 +0900

    [create] <src/container/main.js>

---
+++ artact/src/container/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {UpLoadImage} from '../component';
+function main(){
+    return(
+        <div>
+            <UpLoadImage></UpLoadImage>
+        </div>
+    );
+}
+
+
+
+
+export default main;
