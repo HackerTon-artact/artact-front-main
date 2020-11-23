7ccd6ebabb4176887ef6ddcb234167049ce49975
최종근 <zofqofhtltm2@gmail.com>
2020-11-23 23:38:51 +0900

    [craete] <src/component/exhibition/imageForm.js>

---
+++ artact/src/component/exhibition/imageForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImageInfor from './imageInfor';
+function imageForm(){
+    return(
+        <div>
+            <ImageInfor></ImageInfor>
+        </div>
+    );
+}
+
+export default imageForm;
