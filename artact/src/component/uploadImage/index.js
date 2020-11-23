7170937272bb1945ba0261846fe6725566dec330
최종근 <zofqofhtltm2@gmail.com>
2020-11-23 23:37:22 +0900

    [update] <src/component/uploadImage/index.js>

--- artact/src/component/uploadImage/index.js
+++ artact/src/component/uploadImage/index.js
@@ -1,13 +1,29 @@
-import React,{useState} from 'react';
-function index(){
+import React,{useCallback, useState} from 'react';
+function UpLoadImage(){
+
+    const changeImage = useCallback((e) =>{
+
+    },[]);
     const [image,setImage] = useState('');
 
+    const uploadImage = useCallback((e)=>{
+    
+        e.preventDefault();
+        let reader = new FileReader();
+        let file = e.target.files[0];
+        reader.onloadend = () => {
+            console.log(reader.result);
+            setImage(reader.result);
+        }
+        reader.readAsDataURL(file);        
+    },[]);
     return(
+
         <div>
-            <input type="img" onChange={uploadImage} />
-            <button></button>
+            <img src={image} alt="에러" onChange={changeImage} />
+            <input type="file" onChange={uploadImage} />
         </div>
     );
 }
 
-export default index;
+export default UpLoadImage;
